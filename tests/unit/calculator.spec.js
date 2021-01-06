3b927388498be74010eab3a3153d319cd14a71e6
Mathew-Tomaney <mat.tomaney@gmail.com>
2021-01-06 15:22:37 +0000

    add test for clearClick function

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -8,38 +8,38 @@ describe('App.vue', () => {
   it('enterNum changes running total', () => {
     wrapper.vm.previousTotal = 4
     wrapper.vm.add('5');
-    expect(wrapper.vm.runningTotal).to.equal(9)
+    expect(wrapper.vm.runningTotal).to.equal(9);
   })
 
   it('four plus one is five', () => {
     wrapper.vm.previousTotal = 4
     wrapper.vm.add('1');
-    expect(wrapper.vm.runningTotal).to.equal(5)
+    expect(wrapper.vm.runningTotal).to.equal(5);
   })
 
   it('seven minus four is three', () => {
     wrapper.vm.previousTotal = 7
     wrapper.vm.subtract('4');
-    expect(wrapper.vm.runningTotal).to.equal(3)
+    expect(wrapper.vm.runningTotal).to.equal(3);
   })
   
   it('three multiplied by five is fifteen', () => {
     wrapper.vm.previousTotal = 3
     wrapper.vm.multiply('5');
-    expect(wrapper.vm.runningTotal).to.equal(15)
+    expect(wrapper.vm.runningTotal).to.equal(15);
   })
   
   it('twenty-one divided by seven is three', () => {
     wrapper.vm.previousTotal = 21
     wrapper.vm.divide('7');
-    expect(wrapper.vm.runningTotal).to.equal(3)
+    expect(wrapper.vm.runningTotal).to.equal(3);
   })
   
   it('can concatenate multiple number button clicks', () => {
     wrapper.vm.numberClick('7');
     wrapper.vm.numberClick('2');
     wrapper.vm.numberClick('1');
-    expect(wrapper.vm.runningTotal).to.equal(721)
+    expect(wrapper.vm.runningTotal).to.equal(721);
   })
 
   it('can chain multiple operations', () => {
@@ -48,11 +48,24 @@ describe('App.vue', () => {
     wrapper.vm.operatorClick('+');
     wrapper.vm.numberClick('1');
     wrapper.vm.operatorClick('*');
-    wrapper.vm.numberClick('4')
-    wrapper.vm.operatorClick('/')
-    wrapper.vm.numberClick('2')
-    wrapper.vm.operatorClick('=')
-    expect(wrapper.vm.runningTotal).to.equal(12)
+    wrapper.vm.numberClick('4');
+    wrapper.vm.operatorClick('/');
+    wrapper.vm.numberClick('2');
+    wrapper.vm.operatorClick('=');
+    expect(wrapper.vm.runningTotal).to.equal(12);
+  })
+
+  it('can clear running total without affecting calculation', () => {
+    wrapper.vm.runningTotal = 20
+    wrapper.vm.operatorClick('+');
+    wrapper.vm.numberClick('5');
+    wrapper.vm.operatorClick('-');
+    wrapper.vm.numberClick('4');
+    wrapper.vm.numberClick('0');
+    wrapper.vm.clearClick();
+    wrapper.vm.numberClick('2');
+    wrapper.vm.operatorClick('=');
+    expect(wrapper.vm.runningTotal).to.equal(23);
   })
 
 })

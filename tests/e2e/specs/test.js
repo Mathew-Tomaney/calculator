4aeddde6b3e8fc9f2e9ff011c39db881df179b81
Mathew-Tomaney <mat.tomaney@gmail.com>
2021-01-06 15:42:53 +0000

    add tests for cypress to check number buttons and operators update display

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -7,6 +7,21 @@ describe('calculator', () => {
 
   it('should have working number buttons', () => {
     cy.get('#number2').click();
-    cy.get('.display').should('contain', '2')
-  })
-})
+    cy.get('.display').should('contain', '2');
+  });
+
+  it('number buttons should update display of running total', () => {
+    cy.get('#number4').click();
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '42');
+  });
+
+  it('arithmetical operatotions update display with result', () => {
+    cy.get('#number4').click();
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number8').click();
+    cy.get('#operator_multiply').click();
+    cy.get('.display').should('contain', '50');
+  });
+});
--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -9,38 +9,38 @@ describe('App.vue', () => {
     wrapper.vm.previousTotal = 4
     wrapper.vm.add('5');
     expect(wrapper.vm.runningTotal).to.equal(9);
-  })
+  });
 
   it('four plus one is five', () => {
     wrapper.vm.previousTotal = 4
     wrapper.vm.add('1');
     expect(wrapper.vm.runningTotal).to.equal(5);
-  })
+  });
 
   it('seven minus four is three', () => {
     wrapper.vm.previousTotal = 7
     wrapper.vm.subtract('4');
     expect(wrapper.vm.runningTotal).to.equal(3);
-  })
+  });
   
   it('three multiplied by five is fifteen', () => {
     wrapper.vm.previousTotal = 3
     wrapper.vm.multiply('5');
     expect(wrapper.vm.runningTotal).to.equal(15);
-  })
+  });
   
   it('twenty-one divided by seven is three', () => {
     wrapper.vm.previousTotal = 21
     wrapper.vm.divide('7');
     expect(wrapper.vm.runningTotal).to.equal(3);
-  })
+  });
   
   it('can concatenate multiple number button clicks', () => {
     wrapper.vm.numberClick('7');
     wrapper.vm.numberClick('2');
     wrapper.vm.numberClick('1');
     expect(wrapper.vm.runningTotal).to.equal(721);
-  })
+  });
 
   it('can chain multiple operations', () => {
     wrapper.vm.runningTotal = 0
@@ -53,7 +53,7 @@ describe('App.vue', () => {
     wrapper.vm.numberClick('2');
     wrapper.vm.operatorClick('=');
     expect(wrapper.vm.runningTotal).to.equal(12);
-  })
+  });
 
   it('can clear running total without affecting calculation', () => {
     wrapper.vm.runningTotal = 20
@@ -66,8 +66,7 @@ describe('App.vue', () => {
     wrapper.vm.numberClick('2');
     wrapper.vm.operatorClick('=');
     expect(wrapper.vm.runningTotal).to.equal(23);
-  })
-
-})
+  });
+});
 
 
